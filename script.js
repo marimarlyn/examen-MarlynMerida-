ceb359a5064a411486ad1330e60a7970002131e1
marimarlyn <marlynmerida009@hotmail.com>
2017-03-10 12:20:29 -0600

    marlyn

---
+++ script.js
@@ -0,0 +1,13 @@
+var Marlyn = angular.module (var Marlyn = angular.module("Marlyn[]);
+Marlyn.controller("multiplicacion",function($scope){
+	$scope.cantidad1 = 15
+	$scope.cantidad2 = 20
+
+$scope.resultado = $scope.cantidad1 * $scope.cantidad2;
+}))
+
+
+Marlyn.controller("mensaje",function($scope){
+	$scope.mensaje = Hola 
+
+})
